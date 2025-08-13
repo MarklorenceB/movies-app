f5b4143e3bd3b363ddb5885ebb9ee1a02f7fb228
Mark <marklorenceberon06@gmail.com>
2025-08-13 15:09:45 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
-
-  base: process.env.VITE_BASE_PATH || "/movies-app",
+  base: "./", // ensures assets load correctly in production
 });
